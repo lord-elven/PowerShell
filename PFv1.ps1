9a7128d480c0caab5ed1b2bbfedd175d0ddd4895
lord-elven <47927633+lord-elven@users.noreply.github.com>
2022-04-20 15:02:05 +0100

    Update PFv1.ps1

--- PFv1.ps1
+++ PFv1.ps1
@@ -13,7 +13,7 @@ $VMs = Get-VM | ? {$_.Guest.OSFullName -like "*Windows*"} | Sort Name
 foreach($VM in $VMs){
     Start-Job -Name $VM.Name -ArgumentList $VM.Name -ScriptBlock {
         Import-Module "C:\Program Files (x86)\VMware\Infrastructure\PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1"
-        Connect-VIServer PEG-VC01.insurance.financial.local
+        Connect-VIServer <ESXI Host>
         $Server = Get-VM $args[0]
         Invoke-VMScript -VM $Server -ScriptText "Get-WmiObject Win32_PageFileusage | Select-Object Name"
     }
