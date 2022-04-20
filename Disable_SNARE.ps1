23c3ba0a8e5123615077965cac35654afdbe18c4
lord-elven <47927633+lord-elven@users.noreply.github.com>
2022-04-20 14:50:09 +0100

    Update Disable_SNARE.ps1

--- Disable_SNARE.ps1
+++ Disable_SNARE.ps1
@@ -14,7 +14,7 @@ $VMs = Get-VM | ? {$_.Guest.OSFullName -like "*Windows*"} | Sort Name
 foreach($VM in $VMs){
     Start-Job -Name $VM.Name -ArgumentList $VM.Name -ScriptBlock {
         Import-Module "C:\Program Files (x86)\VMware\Infrastructure\PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1"
-        Connect-VIServer PEG-VC01.insurance.financial.local
+        Connect-VIServer <vSphere Host>
         $Server = Get-VM $args[0]
         Invoke-VMScript -VM $Server -ScriptText "Stop-Service Snare -Force"
         Invoke-VMScript -VM $Server -ScriptText "Set-Service Snare -StartupType Disabled"
@@ -27,7 +27,7 @@ Get-Job
 foreach($VM in $VMs){
     Start-Job -Name $VM.Name -ArgumentList $VM.Name -ScriptBlock {
         Import-Module "C:\Program Files (x86)\VMware\Infrastructure\PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1"
-        Connect-VIServer PEG-VC01.insurance.financial.local
+        Connect-VIServer <vSphere Host>
         $Server = Get-VM $args[0]
         Invoke-VMScript -VM $VM -ScriptText "Set-Service Snare -StartupType Automatic"
         Invoke-VMScript -VM $VM -ScriptText "Start-Service Snare"
